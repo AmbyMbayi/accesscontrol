0bd465006850fb6227fed952e62e6fa5551ae171
AmbyMbayi <ambymbayi54@gmail.com>
2018-09-26 12:36:34 +0300

    add readme file

--- application/models/Accesscontrol_model.php
+++ application/models/Accesscontrol_model.php
@@ -5,12 +5,14 @@ class Accesscontrol_model extends CI_Model{
 
 		
 
-		 $date= $this->db->query("select `USERID`,`username`,`devicename` ,substring(checktime, 1, 10) as date,substring(checktime, 11) as time from accesscontrols where substring(checktime, 1, 10) < '2018-09-31' and substring(checktime, 1, 10) > '2018-09-01' group by time");
+		 /*$date= $this->db->query("select `USERID`,`username`,`devicename` ,substring(checktime, 1, 10) as date,substring(checktime, 11) as time from accesscontrols where substring(checktime, 1, 10) < '2018-09-31' and substring(checktime, 1, 10) > '2018-09-01' group by time");
 
 		return $date;
+		*/
+		$date  = $this->db->query("SELECT `USERID`,`username`,`devicename`, substring(CHECKTIME,1,10)as date ,substring(CHECKTIME,11) AS time FROM `accesscontrols` WHERE `CHECKTIME` LIKE '2018-09-12%' GROUP BY USERID");
 		
+		return $date;
 
-		
 	
 
  	/* $date= $this->db->query("SELECT USERID, username, devicename,  MAX(CHECKTIME) AS   date, substring(checktime, 11) as time FROM accesscontrols WHERE DATE(CHECKTIME) > '2018-09-01' AND DATE(CHECKTIME) >'2018-09-12' GROUP BY USERID");
